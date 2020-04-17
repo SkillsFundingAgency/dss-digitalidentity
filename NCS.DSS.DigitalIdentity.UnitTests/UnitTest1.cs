aacab5a051ae2cdd99d2ca5f54fa35680f304ed9
mofais <raymar786@gmail.com>
2020-04-17 09:38:26 +0100

    NCSD-3378-New-Repo-dss-digitalidentity

---
+++ NCS.DSS.DigitalIdentity.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NCS.DSS.DigitalIdentity.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
