e698bfc68d96687b96dcb3d2e82e05e96cde2ae3
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-05-07 12:48:24 +0100

    added post identity service

---
+++ NCS.DSS.DigitalIdentity/PostDigitalIdentityHttpTrigger/Service/PostDigitalIdentityHttpTriggerService.cs
@@ -0,0 +1,25 @@
+﻿using NCS.DSS.DigitalIdentity.Cosmos.Provider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.DigitalIdentity.PostDigitalIdentityHttpTrigger.Service
+{
+    public class PostDigitalIdentityHttpTriggerService : IPostDigitalIdentityTriggerService
+    {
+        private readonly IDocumentDBProvider _documentDbProvider;
+
+        public PostDigitalIdentityHttpTriggerService(IDocumentDBProvider documentDbProvider)
+        {
+            _documentDbProvider = documentDbProvider;
+        }
+
+        public async Task<Models.DigitalIdentity> GetIdentityForCustomerAsync(Guid customerId)
+        {
+            var identity = await _documentDbProvider.GetIdentityForCustomerAsync(customerId);
+
+            return identity;
+        }
+    }
+}
