81d108d06bb583719dc295abef8b5ce87dcc1f30
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-05-13 14:35:17 +0100

    removed service code to resolve build issues

--- NCS.DSS.DigitalIdentity/ServiceBus/DigitalIdentitServiceBusClient.cs
+++ NCS.DSS.DigitalIdentity/ServiceBus/DigitalIdentitServiceBusClient.cs
@@ -5,9 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using DFC.Common.Standard.ServiceBusClient.Models;
 using Newtonsoft.Json;
-using Microsoft.ServiceBus;
-using Microsoft.ServiceBus.Messaging;
-
 
 namespace NCS.DSS.DigitalIdentity.ServiceBus
 {
@@ -32,27 +29,32 @@ namespace NCS.DSS.DigitalIdentity.ServiceBus
 
         private async Task SendMessageAsync(Models.DigitalIdentity digitalIdentity, string reqUrl, string message)
         {
-            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, AccessKey);
-            var messagingFactory = await MessagingFactory.CreateAsync(BaseAddress, tokenProvider);
-            var sender = await messagingFactory.CreateMessageSenderAsync(QueueName);
+            //var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, AccessKey);
+            //var messagingFactory = await MessagingFactory.CreateAsync(BaseAddress, tokenProvider);
+            //var sender = await messagingFactory.CreateMessageSenderAsync(QueueName);
 
-            var messageModel = new MessageModel()
-            {
-                TitleMessage = message,
-                CustomerGuid = digitalIdentity.CustomerId,
-                LastModifiedDate = digitalIdentity.LastModifiedDate,
-                URL = reqUrl + "identity/" + digitalIdentity.IdentityID,
-                IsNewCustomer = false,
-                TouchpointId = digitalIdentity.LastModifiedTouchpointId
-            };
+            //var messageModel = new MessageModel()
+            //{
+            //    TitleMessage = message,
+            //    CustomerGuid = digitalIdentity.CustomerId,
+            //    LastModifiedDate = digitalIdentity.LastModifiedDate,
+            //    URL = reqUrl + "identity/" + digitalIdentity.IdentityID,
+            //    IsNewCustomer = false,
+            //    TouchpointId = digitalIdentity.LastModifiedTouchpointId
+            //};
 
-            var msg = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel))))
-            {
-                ContentType = "application/json",
-                MessageId = digitalIdentity.IdentityID + " " + DateTime.UtcNow //TODO : Should this be IdentityId or CustomerId ?
-            };
+            //var msg = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel))))
+            //{
+            //    ContentType = "application/json",
+            //    MessageId = digitalIdentity.IdentityID + " " + DateTime.UtcNow //TODO : Should this be IdentityId or CustomerId ?
+            //};
 
-            await sender.SendAsync(msg);
+            //await sender.SendAsync(msg);
+
+            // TODO : above was commented out to fix build issue
+            // see https://dev.azure.com/sfa-gov-uk/CDS%202.0/_build/results?buildId=316531&view=logs&j=34dafb0e-df1f-525d-bac3-6f55caa340ef&t=4c012cd0-3d6d-5ee6-5990-7147ca18d932&l=49
+
+            await Task.Delay(1);
         }
     }
 }
