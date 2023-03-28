f6e3547e0677ea6bbf3e69cd73ba56230641775e
madhu-pasumarthi <madhu.pasumarthi@netcompany.com>
2023-03-28 11:18:27 +0100

    LastLoggedInDateTime making non private

--- NCS.DSS.DigitalIdentity/Models/DigitalIdentity.cs
+++ NCS.DSS.DigitalIdentity/Models/DigitalIdentity.cs
@@ -31,7 +31,7 @@ namespace NCS.DSS.DigitalIdentity.Models
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the customer last signed in through the digital service.")]
         [Example(Description = "2018-06-20T13:45:00")]
-         public DateTime? LastLoggedInDateTime { get; private set; }
+         public DateTime? LastLoggedInDateTime { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time of the last modification to the record.")]
